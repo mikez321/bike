9449fb3ed282abad16d4749e25da55184e231dae
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-20 16:57:48 -0700

    Add axle type choices to bike model

--- bike/models.py
+++ bike/models.py
@@ -13,6 +13,10 @@ class Bike(models.Model):
         (5, 'E-Bike'),
         (6, 'Other'),
     ]
+    AXLE_TYPE_CHOICES = [
+        (1, 'QR'),
+        (2, 'Thru'),
+    ]
     make = models.CharField(max_length=30, blank=False, null=False)
     model = models.CharField(max_length=30, blank=False, null=False)
     type = models.IntegerField(
@@ -21,6 +25,16 @@ class Bike(models.Model):
         null=False
     )
     description = models.TextField(max_length=200, blank=True, null=True)
+    f_axle = models.IntegerField(
+        choices = AXLE_TYPE_CHOICES,
+        blank=False,
+        null=False,
+    )
+    r_axle = models.IntegerField(
+        choices = AXLE_TYPE_CHOICES,
+        blank=False,
+        null=False,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
