53e1bf0faac0c2c767658b3b00167518746a38f0
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-20 22:07:17 -0700

    Test happy path for all CRUD functionalty of bikes

--- bike/test/test_bike_endpoints.py
+++ bike/test/test_bike_endpoints.py
@@ -29,5 +29,65 @@ class BikeEndpointTest(TestCase):
         response = self.client.get('/bikes/')
         self.assertEqual(response.status_code, 200)
 
-        num_bikes = Bike.objects.all().count()
+        num_bikes = Bike.objects.count()
         self.assertEqual(len(response.data), num_bikes)
+
+    def test_bike_detail(self):
+        """GET request for a bike by pk."""
+        response = self.client.get('/bikes/2/')
+        self.assertEqual(response.status_code, 200)
+
+        bike1 = Bike.objects.first()
+        bike2 = Bike.objects.last()
+        self.assertNotEqual(response.data['id'], bike1.id)
+        self.assertEqual(response.data['id'], bike2.id)
+
+    def test_bike_creation(self):
+        """POST rquest to add a bike."""
+        num_starting_bikes = Bike.objects.count()
+        self.assertEqual(num_starting_bikes, 2)
+
+        new_bike_attributes = {
+            'make': 'Specialized',
+            'model': 'Enduro',
+            'type': 2,
+            'description': 'Grip it and rip it',
+            'f_axle': 2,
+            'r_axle': 2,
+        }
+        response = self.client.post(
+            '/bikes/', new_bike_attributes, format='json'
+        )
+
+        self.assertEqual(response.status_code, 201)
+        current_num_bikes = Bike.objects.count()
+        self.assertEqual(current_num_bikes, 3)
+
+    def test_bike_update(self):
+        """PATCH/PUT request to update a bike."""
+        bike = Bike.objects.first()
+        self.assertEqual(bike.description, "It's a hardtail 29er.")
+        bike_updates = {
+            'make': 'Specialized',
+            'model': 'Chisel',
+            'type': 2,
+            'description': "It's all Flexy Jackson.",
+            'f_axle': 2,
+            'r_axle': 2,
+        }
+        response = self.client.patch(
+            '/bikes/1/', bike_updates, format='json'
+        )
+        self.assertEqual(response.status_code, 200)
+        updated_bike = Bike.objects.first()
+        self.assertEqual(updated_bike.description, "It's all Flexy Jackson.")
+
+    def test_bike_delete(self):
+        """DELETE request for bikes."""
+        num_starting_bikes = Bike.objects.count()
+        self.assertEqual(num_starting_bikes, 2)
+
+        response = self.client.delete('/bikes/1/')
+        self.assertEqual(response.status_code, 204)
+        updated_num_bikes = Bike.objects.count()
+        self.assertEqual(updated_num_bikes, 1)
