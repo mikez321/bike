abea85519a36cf12393dee6e92c3cd20a669906f
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-20 20:53:15 -0700

    Write custom validators for rear wheel serializer

--- wheel/serializers.py
+++ wheel/serializers.py
@@ -1,6 +1,7 @@
 """Wheel serializer."""
 from wheel.models import FrontWheel, RearWheel
 from rest_framework import serializers
+from IPython import embed
 
 
 class FrontWheelSerializer(serializers.ModelSerializer):
@@ -13,6 +14,19 @@ class FrontWheelSerializer(serializers.ModelSerializer):
 
 class RearWheelSerializer(serializers.ModelSerializer):
     """Serializer for RearWheel object."""
+
+    def validate(self, data):
+        """Declare extra validations."""
+        if (data.get('fixed') or data.get('single_speed_only')
+                and data['driver'] != 6):
+            raise serializers.ValidationError(
+                "Invalid combination of rear hub drivers."
+            )
+        if data.get('fixed') and data.get('single_speed_only'):
+            raise serializers.ValidationError(
+                "Fixed hubs are not the same as single speed hubs."
+            )
+        return data
 
     class Meta:
         model = RearWheel
--- wheel/test/test_rear_wheel_serializer.py
+++ wheel/test/test_rear_wheel_serializer.py
@@ -3,7 +3,7 @@
 from django.test import TestCase
 from wheel.models import RearWheel
 from wheel.serializers import RearWheelSerializer
-
+from IPython import embed
 
 class RearWheelSerializerTest(TestCase):
     """Testing for Front Wheel serializer."""
@@ -46,3 +46,20 @@ class RearWheelSerializerTest(TestCase):
         self.assertEqual(set(data.keys()), set(expected_keys))
         for key in omitted_keys:
             self.assertFalse(key in data.keys())
+
+    def test_custom_validations(self):
+        """Test for custom validations."""
+        self.r_wheel_attributes['fixed'] = True
+        serializer = RearWheelSerializer(
+            instance=self.f_wheel,
+            data=self.r_wheel_attributes
+        )
+        self.assertFalse(serializer.is_valid())
+        self.assertTrue('non_field_errors' in serializer.errors.keys())
+        self.r_wheel_attributes['single_speed_only'] = True
+        serializer = RearWheelSerializer(
+            instance=self.f_wheel,
+            data=self.r_wheel_attributes
+        )
+        self.assertFalse(serializer.is_valid())
+        self.assertTrue('non_field_errors' in serializer.errors.keys())
