19f3d0c271014b691cf264862f7e6496f022e61c
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-23 09:30:34 -0700

    Update CLI actions to add wheels to bikes.

--- cli/cli.py
+++ cli/cli.py
@@ -54,8 +54,19 @@ class WheelInstaller():
     """Install a wheel on a bike through the API."""
 
     def install(self, wheel, bike):
-        """Provided a wheel install it on the bike or handle error."""
-        response = requests.put('http://localhost:8000/wheels/rear')
+        """Install a wheel on the bike or handle error."""
+        bike_info = bike.__dict__
+        bike_info['brake_type'] = bike.brakes
+        location = 'f_wheel' if wheel.wheel_type == 'front' else 'r_wheel'
+        bike_info[location] = wheel.db_ref
+        response = requests.patch(
+            f"http://localhost:8000/bikes/{bike.db_ref}/",
+            bike_info,
+        )
+        if response.status_code == 200:
+            return "Install went smooth!"
+        else:
+            return "Something went wrong..."
 
 
 bikefetcher = BikeFetcher()
@@ -73,16 +84,27 @@ for option, bike in bike_dict.items():
 print()
 bike_choice = input("Choose a bike: ")
 bike = bike_dict[int(bike_choice)]
-print(f"Bike: {bike.make} {bike.model}")
-print(f"-Type: {bike.bike_type.capitalize()}")
-print(f"-Brakes: {bike.brake_type.capitalize()}")
-print(f"-Front Axle: {bike.front_axle_type.capitalize()}")
-print(f"-Rear_Axle: {bike.rear_axle_type.capitalize()}")
-print()
-print("Available Wheels:")
-for option, wheel in wheel_dict.items():
-    print(f"{option}:\t{wheel.manufacturer} {wheel.model} ({wheel.wheel_type}/{wheel.axle_type})")
-wheel_choice = input("Choose a wheel to install: ")
-wheel = wheel_dict[int(wheel_choice)]
-print()
-print(f"Installing {wheel.manufacturer} {wheel.model} on {bike.make} {bike.model}")
+while True:
+    if bike.f_wheel is not None and bike.r_wheel is not None:
+        print("One bike... two wheels... all set!")
+        break
+    else:
+        print(f"Bike: {bike.make} {bike.model}")
+        print(f"-Type: {bike.bike_type.capitalize()}")
+        print(f"-Brakes: {bike.brake_type.capitalize()}")
+        print(f"-Front Axle: {bike.front_axle_type.capitalize()}")
+        print(f"-Rear_Axle: {bike.rear_axle_type.capitalize()}")
+        current_f_wheel = 'installed' if bike.f_wheel else ' -- '
+        print(f"-Front Wheel: {current_f_wheel.capitalize()}")
+        current_r_wheel = 'installed' if bike.r_wheel else ' -- '
+        print(f"-Rear Wheel: {current_r_wheel.capitalize()}")
+        print()
+        print("Available Wheels:")
+        for option, wheel in wheel_dict.items():
+            print(f"{option}:\t{wheel.manufacturer} {wheel.model} ({wheel.wheel_type}/{wheel.axle_type})")
+        wheel_choice = input("Choose a wheel to install: ")
+        wheel = wheel_dict[int(wheel_choice)]
+        print()
+        print(f"Installing {wheel.manufacturer} {wheel.model} on {bike.make} {bike.model}")
+        wheelinstaller = WheelInstaller()
+        print(wheelinstaller.install(wheel, bike))
