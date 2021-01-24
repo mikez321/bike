a13ffb8a9b1fe28c6fbed6a746c82aaddbc4b1d7
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-24 14:21:52 -0700

    Update settings for AWS

--- api/settings.py
+++ api/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['mike-bike.eba-zn7bvz6q.us-west-2.elasticbeanstalk.com']
 
