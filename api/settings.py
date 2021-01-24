2f7dd2bae51f0552cf09396da4a1c8e3d74ccbba
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-24 13:13:33 -0700

    update django settings

--- api/settings.py
+++ api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['bike-api.eba-bzphjieb.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['mike-bike.eba-zn7bvz6q.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
