24a5313c8bf9938c10d3a766f81b0308457ddae5
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-24 14:51:55 -0700

    Remove dotenv imports

--- api/settings.py
+++ api/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-# Load secrets and hidden keys
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
