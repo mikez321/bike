ea29471f96ef4c1a5284ed6a205385a6272f4a43
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-24 12:37:19 -0700

    Update allowed hosts to new elastic beanstalk location

--- api/settings.py
+++ api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bike-api.eba-bzphjieb.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
